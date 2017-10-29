2467e96225971ff72a03deb75aac558c8066814c
William Ramirez <mercan01@gmail.com>
2017-10-28 22:04:23 -0400

    Initial commit

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TestController } from '../controllers/test.controller';
+
 
 @Module({
     modules: [],
+    controllers: [TestController]
 })
 export class ApplicationModule {}
